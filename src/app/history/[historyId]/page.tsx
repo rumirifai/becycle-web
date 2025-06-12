c517d417cded3d1ad72b086af1a09fc206c85b24
Rumi Rifai <rumirifai96@gmail.com>
2025-06-12 15:24:47 +0700

    Fix: Fix history id page

--- src/app/history/[historyId]/page.tsx
+++ src/app/history/[historyId]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, FC } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import MainLayout from '@/components/MainLayout';
